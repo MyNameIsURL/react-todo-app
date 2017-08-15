a775dfd23e5b86689358c816dd2ec162d0ed8341
Patrick Ayres <briceayres@gmail.com>
2017-08-14 23:02:10 -0500

    remove todos feature added

--- src/App.js
+++ src/App.js
@@ -9,19 +9,30 @@ class App extends Component {
     super(props);
     this.state = {
       todos: [
-        "Make dinner tonight!",
-        "Fold the laundry.",
-        "Learn to make a React app!"
-      ]
+        {id: 0, text: "Make dinner tonight!"},
+        {id: 1, text: "Fold the laundry."},
+        {id: 2, text: "Learn to make a React app!"}
+      ],
+      nextId: 3
     };
 
     this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
   }
 
-  addTodo(todo) {
+  addTodo(todoText) {
     let todos = this.state.todos.slice();
-    todos.push(todo);
-    this.setState({ todos: todos });
+    todos.push({id: this.state.nextId, text: todoText});
+    this.setState({
+      todos: todos,
+      nextId: ++this.state.nextId
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+        todos: this.state.todos.filter((todo, index) => todo.id !== id)
+      });
   }
 
   render() {
@@ -29,10 +40,12 @@ class App extends Component {
       <div className="App">
         <div className="todo-wrapper">
           <Header />
-          <TodoInput todoText="" addTodo={this.addTodo} todos={this.state.todos}/>
+          <TodoInput todoText="" addTodo={this.addTodo} />
           <ul>
             {
-              this.state.todos.map((todo) => <TodoItem todo={todo}/>)
+              this.state.todos.map((todo) => {
+                return <TodoItem todo={todo} key={todo.id} id={todo.id} removeTodo={this.removeTodo}/>
+              })
             }
           </ul>
         </div>
--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -6,10 +6,14 @@ export default class TodoItem extends React.Component {
     super(props);
   }
 
+  removeTodo(id) {
+    this.props.removeTodo(id);
+  }
+
   render() {
     return (
       <div className="todoWrapper">
-        <button className="removeTodo">remove</button> {this.props.todo}
+        <button className="removeTodo" onClick={(e)=> this.removeTodo(this.props.id) }>remove</button>{this.props.todo.text}
       </div>
     );
   }
